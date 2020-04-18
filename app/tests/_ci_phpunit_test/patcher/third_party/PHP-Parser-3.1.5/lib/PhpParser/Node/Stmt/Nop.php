2e4f7a0d7607091461730ab7ca96955bd29768e5
cocoytech <55616292+cocoytech@users.noreply.github.com>
2020-04-19 06:59:46 +0800

    Feat(base): (#9)
    * Feat(base): CI3 template from kenji ci
    
    * Feat(base): add composer to base code
    
    * Chore(composer): add composer file #5
    
    * Doc(codeowner): add cocoytech as default codeowner and approver
    
    Co-authored-by: cocoytech <francisco.abayon@rafi.ph>

---
+++ app/tests/_ci_phpunit_test/patcher/third_party/PHP-Parser-3.1.5/lib/PhpParser/Node/Stmt/Nop.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PhpParser\Node\Stmt;
+
+use PhpParser\Node;
+
+/** Nop/empty statement (;). */
+class Nop extends Node\Stmt
+{
+    public function getSubNodeNames()
+    {
+        return array();
+    }
+}
