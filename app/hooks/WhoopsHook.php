ba5fec26d8f5082dbd7bd1d4d715a443f43e5d3f
cocoytech <francisco.abayon@rafi.ph>
2020-04-19 23:52:55 +0800

    Fix untrack commits

---
+++ app/hooks/WhoopsHook.php
@@ -0,0 +1,8 @@
+<?php
+class WhoopsHook {
+    public function bootWhoops() {
+        $whoops = new \Whoops\Run;
+        $whoops->pushHandler(new Whoops\Handler\PrettyPageHandler());
+        $whoops->register();
+    }
+}
