02cee70ceaecd6a5d9a11e741b155d9bf1eb4578
cocoytech <francisco.abayon@rafi.ph>
2020-04-29 12:34:54 +0800

    Random

--- app/config/hooks.php
+++ app/config/hooks.php
@@ -1,17 +1,4 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-/*
-| -------------------------------------------------------------------------
-| Hooks
-| -------------------------------------------------------------------------
-| This file lets you define "hooks" to extend CI without hacking the core
-| files.  Please see the user guide for info:
-|
-|	https://codeigniter.com/user_guide/general/hooks.html
-|
-*/
-<?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
--- app/hooks/DotEnvHook.php
+++ app/hooks/DotEnvHook.php
@@ -3,9 +3,6 @@ class DotEnvHook
 {
     public function LoadEnvironment()
     {
-        //$dotenv = new Dotenv\Dotenv();
-        $dotenv = Dotenv\Dotenv::createImmutable(ROOT);
-        $dotenv->load();
-        $dotenv->required(['S3_BUCKET', 'SECRET_KEY', 'BASE_LINK']);
+      
     }
 }
